178f356332371cb4000226438c72874765bf1cd6
El profe <al.sepulvedam@profesor.duoc.cl>
2024-10-11 12:27:14 -0300

    guardar con ejemplo simple

--- src/app/pages/detalle-comida/detalle-comida.page.ts
+++ src/app/pages/detalle-comida/detalle-comida.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Receta } from 'src/app/interfaces/icomidas';
+import { AlmacenamientoService } from 'src/app/services/almacenamiento.service';
 import { DatosService } from 'src/app/services/datos.service';
 
 @Component({
@@ -11,7 +12,11 @@ import { DatosService } from 'src/app/services/datos.service';
 export class DetalleComidaPage implements OnInit {
 
   listareceta:Receta[]=[]
-  constructor(private router:Router, private srv:DatosService) { }
+  constructor(private router:Router, 
+    private srv:DatosService,
+    private db:AlmacenamientoService,
+    ) { }
+
   public actionSheetButtons = [
     {
       text: 'Agregar a Favoritos',
@@ -44,6 +49,21 @@ export class DetalleComidaPage implements OnInit {
       });
     }
 
+  }
+
+  /*metodos de prueba*/
+  save(){
+
+    this.db.guardar('100',"hola padelman");
+  }
+
+  cargar(){
+    let clave:string="100"
+    let valor=this.db.leer(clave);
+    valor.then(dato=>{
+      console.log("DATOS DESDE STORAGE: " + dato);
+    })
+    
   }
 
 }
--- src/app/services/almacenamiento.service.ts
+++ src/app/services/almacenamiento.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlmacenamientoService {
+  private _storage: Storage | null = null;
 
-  constructor() { }
+  constructor(private storage: Storage) {
+    this.init();
+   }
+
+   async init() {
+    // If using, define drivers here: await this.storage.defineDriver(/*...*/);
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+
+  
+  public guardar(key: string, value: any) {
+    this._storage?.set(key, value);
+  }
+
+  public async leer(key:string){
+    return await this._storage?.get(key);
+  }
+
 }
